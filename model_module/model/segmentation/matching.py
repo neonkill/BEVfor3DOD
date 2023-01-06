9d6347c97c82e1912b597752322e8de22129d1d2
yelin2 <yelin120993@gmail.com>
2023-01-06 04:20:41 +0000

    road_lr4e-3_pe_q25_cvt

--- model_module/model/segmentation/matching.py
+++ model_module/model/segmentation/matching.py
@@ -3,7 +3,69 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch import Tensor
+from einops import rearrange
 
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, dim, heads, qkv_bias, norm=nn.LayerNorm):
+        super().__init__()
+
+        dim_head = int(dim/heads)
+        self.scale = dim_head ** -0.5
+
+        self.heads = heads
+        self.dim_head = dim_head
+
+        self.to_q = nn.Sequential(norm(dim), nn.Linear(dim, heads * dim_head, bias=qkv_bias))
+        self.to_k = nn.Sequential(norm(dim), nn.Linear(dim, heads * dim_head, bias=qkv_bias))
+        self.to_v = nn.Sequential(norm(dim), nn.Linear(dim, heads * dim_head, bias=qkv_bias))
+
+        self.proj = nn.Linear(heads * dim_head, dim)
+        self.prenorm = norm(dim)
+        self.mlp = nn.Sequential(nn.Linear(dim, 2 * dim), nn.GELU(), nn.Linear(2 * dim, dim))
+        self.postnorm = norm(dim)
+
+    def forward(self, q, k, v):
+        """
+        q: (b d H W)
+        k: (b n d h w)
+        v: (b n d h w)
+        """
+        _, _, H, W = q.shape
+
+        # Move feature dim to last for multi-head proj
+        q = rearrange(q, 'b d H W -> b (H W) d')
+        k = rearrange(k, 'b n d h w -> b (n h w) d')
+        v = rearrange(v, 'b n d h w -> b (n h w) d')
+
+        # Project with multiple heads
+        q = self.to_q(q)                                # b (H W) (heads dim_head)
+        k = self.to_k(k)                                # b (n h w) (heads dim_head)
+        v = self.to_v(v)                                # b (n h w) (heads dim_head)
+
+        # Group the head dim with batch dim
+        q = rearrange(q, 'b ... (m d) -> (b m) ... d', m=self.heads, d=self.dim_head)   # (b m) (H W) d
+        k = rearrange(k, 'b ... (m d) -> (b m) ... d', m=self.heads, d=self.dim_head)   # (b m) (n h w) d
+        v = rearrange(v, 'b ... (m d) -> (b m) ... d', m=self.heads, d=self.dim_head)   # (b m) (n h w) d
+
+        # Dot product attention along cameras
+        dot = self.scale * torch.einsum('b q d, b k d -> b q k', q, k)
+        att = dot.softmax(dim=-1)
+
+        # Combine values (image level features).
+        a = torch.einsum('b q k, b k d -> b q d', att, v)
+        a = rearrange(a, '(b m) ... d -> b ... (m d)', m=self.heads, d=self.dim_head)
+
+        # Combine multiple heads
+        z = self.proj(a)
+
+        z = self.prenorm(z)
+        z = z + self.mlp(z)
+        z = self.postnorm(z)
+        z = rearrange(z, 'b (H W) d -> b d H W', H=H, W=W)
+
+        return z
 
 
 class TransformerDecoderLayer(torch.nn.Module):
