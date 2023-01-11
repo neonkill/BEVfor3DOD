05981b8fc3ede7a93a6a66299d7212bbcf61598a
yelin2 <yelin120993@gmail.com>
2023-01-11 02:50:03 +0000

    add inference timecheck code

---
+++ scripts/inference_timecheck.py
@@ -0,0 +1,60 @@
+
+'''
+measure models inference time
+'''
+
+import hydra
+import torch
+import numpy as np
+from tqdm import tqdm
+# from pathlib import Path
+
+from utils import setup_config, setup_network
+
+CONFIG_PATH = '/usr/src/CV_For_Autonomous_Driving/config'
+CONFIG_NAME = 'default_config.yaml'
+
+
+@hydra.main(config_path=CONFIG_PATH, config_name=CONFIG_NAME)
+def main(cfg):
+    setup_config(cfg)
+    model = setup_network(cfg)
+    device = torch.device("cuda")
+    model.to(device)
+
+    # h = cfg.data
+    image = torch.randn(1, 6, 3, cfg.data.image.h, cfg.data.image.w, dtype=torch.float).to(device)
+    I = torch.randn((1, 6, 3, 3), dtype=torch.float).to(device)
+    E = torch.randn((1, 6, 4, 4), dtype=torch.float).to(device)
+
+    dummy_input = {'image': image,
+                    'intrinsics': I,
+                    'extrinsics': E}
+
+
+    # INIT LOGGERS
+    starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    repetitions = 1000
+    timings=np.zeros((repetitions,1))
+
+    #GPU-WARM-UP
+    for _ in range(100):
+        _ = model(dummy_input)
+
+    # MEASURE PERFORMANCE
+    with torch.no_grad():
+        for rep in tqdm(range(repetitions)):
+
+            starter.record()
+            _ = model(dummy_input)
+            ender.record()
+            torch.cuda.synchronize()
+            time = starter.elapsed_time(ender)
+            timings[rep] = time
+
+    bb = np.sum(timings) / repetitions
+
+    print(f'inference time: {bb:.2f} ms  {1000/bb:.2f} fps')
+
+if __name__ == '__main__':
+    main()
